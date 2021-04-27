f601e4056a6225077a162b8b9103daeb75194f80
Duanckham <duanckham@gmail.com>
2021-04-27 23:46:04 +0800

    MISC

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const caches = {};
 const API = 'https://configin.com';
 
 function decode(obj) {
-  let result = {};
+  const result = {};
   for (k in obj) {
     result[k] = Buffer.from(obj[k], 'hex');
   }
@@ -16,29 +16,27 @@ function decode(obj) {
 };
 
 function pull(hash, accessKey, verbose) {
-  let date = moment().format('YYYYMMDDHH');
-  let token = hasha(accessKey + hasha(accessKey + date));
-  let updated = 0;
+  const date = moment().format('YYYYMMDDHH');
+  const token = hasha(accessKey + hasha(accessKey + date));
+  const updated = caches[hash] ? caches[hash].updated : 0;
 
-  if (caches[hash]) {
-    updated = caches[hash].updated;
-  }
-
-  let options = {
+  const options = {
     url: `${API}/api/sdk/config/${hash}/${token}?updated=${updated}`,
-    json: true
+    json: true,
+    gzip: true,
+    timeout: 5 * 1000,
   };
 
   request(options, (err, _, body) => {
     if (err || !body) {
       if (verbose) {
-        console.error('Request configin API failed, error:', err);
+        console.error('request configin API failed, error:', err);
       }
-      
+
       return;
     }
 
-    let { message, data } = body;
+    const { message, data } = body;
 
     if (message === 'success') {
       if (!data.cipher) {
@@ -48,7 +46,7 @@ function pull(hash, accessKey, verbose) {
       eccrypto.decrypt(Buffer.from(accessKey, 'hex'), decode(data.cipher)).then(config => {
         caches[hash] = {
           config,
-          updated: data.updated
+          updated: data.updated,
         };
       });
     }
@@ -57,7 +55,7 @@ function pull(hash, accessKey, verbose) {
 
 exports.use = (hash, accessKey, parser, verbose) => {
   schedule.scheduleJob('*/10 * * * * *', async () => {
-    pull(hash, accessKey, verbose)
+    pull(hash, accessKey, verbose);
   });
 
   return () => {
